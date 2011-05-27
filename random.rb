f6dca8f4a9925f54de736d76dba70dd357f32a7e
Andrew Horsman <minirobotics@gmail.com>
2011-05-27 13:48:55 -0400

    Sometimes Wikipedia serves a page improperly so I've added an exception catch skip.  Added data logging for the random script.

--- random.rb
+++ random.rb
@@ -7,9 +7,13 @@ require File.dirname(__FILE__) + "/run"
 require 'net/http'
 require 'uri'
 
+def e(text)
+  text.to_s.gsub(",", "\,")
+end
+
 def add_data(start, trail, quantity, finished)
   output = File.open("data.csv", "a")
-  output.puts "#{start},#{trail.length},#{finished ? "yes":"false"},#{trail.join(" -> ")}"
+  output.puts "#{e(start)},#{e(trail.length)},#{finished ? "yes":"false"},#{e(trail.join(" -> "))}"
 end
 
 def get_new_link
@@ -19,13 +23,20 @@ end
 
 largest = { :quantity => 0, :page => nil }
 loop {
-  wiki_page = get_new_link
-  g = ExtendedMind.new(get_new_link)
-  add_data(wiki_page, g.breadcrumbs, g.breadcrumbs.length, g.reached_end)
-  if g.breadcrumbs.length > largest[:quantity]
-    largest[:quantity] = g.breadcrumbs.length
-    largest[:page] = wiki_page
+  begin
+    wiki_page = get_new_link
+    g = ExtendedMind.new(get_new_link)
+    add_data(wiki_page, g.breadcrumbs, g.breadcrumbs.length, g.reached_end)
+    if g.breadcrumbs.length > largest[:quantity]
+      largest[:quantity] = g.breadcrumbs.length
+      largest[:page] = wiki_page
 
-    puts "New largest! (#{largest[:quantity]} clicks from #{wiki_page} to Philosophy)."
+      puts "New largest! (#{largest[:quantity]} clicks from #{wiki_page} to Philosophy)."
+    end
+  rescue SystemExit, Interrupt
+    raise
+  rescue Exception => e
+    puts "Invalid page occurred, skipped."
+    puts e
   end
 }
